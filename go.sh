0b91c03671e11f958b65ed1e73bdbd8a770e76d4
harry zhang <harry zhang@localhost>
2018-02-01 17:04:25 -0500

    add docker-compose.yml file

--- go.sh
+++ go.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 ./bin/create-swarm-mode-cluster.sh create nodes
-./bin/create-swarm-mode-cluster.sh create swarm
+./bin/create-swarm-mode-cluster.sh create swarm 1
 docker-machine ls
 docker-machine ip node-1
 docker-machine ip node-2
 docker-machine ip node-3
+
+docker-machine scp src/*.yml node-1:.
+
+docker-machine ssh node-1 "docker stack deploy -c docker-compose.yml getstartedlab"
