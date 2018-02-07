0e55e21ffd154f2536b379a7483ba0f1d46b70a6
harry zhang <harry zhang@localhost>
2018-02-07 10:17:54 -0500

    add spark part

---
+++ gospark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+currentfodler=$(pwd -P)
+cd "$(dirname "$0")"
+
+./bin/create-swarm-mode-cluster.sh create nodes
+./bin/create-swarm-mode-cluster.sh create swarm 1
+
+docker-machine scp src/\*.yml node-1:.
+docker-machine scp -r spark/conf node-1:.
+docker-machine ssh node-1 "mkdir ~/data"
+docker-machine ssh node-1 "mkdir ~/data2"
+docker-machine ssh node-1 "docker pull gettyimages/spark"
+docker-machine ssh node-1 "docker stack deploy -c docker-compose_spark.yml getstartedlab"
+
+cd ${currentfodler}
+
+
+#docker stack deploy -c docker-compose_spark.yml getstartedlab1
+#docker stack rm getstartedlab1
+
+#https://github.com/gettyimages/docker-spark
+#https://github.com/vkorukanti/spark-docker-compose/blob/master/docker-compose.yml
+#https://medium.com/@ivanermilov/scalable-spark-hdfs-setup-using-docker-2fd0ffa1d6bf
+#https://docs.docker.com/compose/networking/#update-containers
+#https://docs.docker.com/engine/userguide/networking/default_network/container-communication/
