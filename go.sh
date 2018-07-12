ccd7434658ee8de8176122be6eccaecbe38cac4e
Harry <Harryyhzhang@gmail.com>
2018-07-12 12:36:31 -0400

    change go.sh to add vagrant script

--- go.sh
+++ go.sh
@@ -2,24 +2,40 @@
 currentfodler=$(pwd -P)
 cd "$(dirname "$0")"
 
-./bin/create-swarm-mode-cluster.sh create nodes
-./bin/create-swarm-mode-cluster.sh create swarm 1
-docker-machine ls
-docker-machine ip node-1
-docker-machine ip node-2
-docker-machine ip node-3
+#./bin/create-swarm-mode-cluster.sh create nodes
+#./bin/create-swarm-mode-cluster.sh create swarm 1
+ 
 
-./bin/getconfig.sh
+#./bin/getconfig.sh
 
-docker-machine ssh node-1 "mkdir ./haproxy"
-docker-machine scp src/\*.yml node-1:.
-docker-machine scp haproxy/result_haproxy.cfg node-1:./haproxy/haproxy.cfg
+vagrant.exe up
 
-docker-machine ssh node-1 "docker stack deploy -c docker-compose.yml getstartedlab"
+docker-machine create \
+      --driver "generic" \
+      --generic-ip-address 192.168.33.10 \
+      --generic-ssh-user vagrant \
+      --generic-ssh-key .vagrant/machines/host1/virtualbox/private_key \
+      --generic-ssh-port 22 \
+      host1
 
-route delete 172.18.0.0
-route add 172.18.0.0 MASK 255.255.255.0 `docker-machine ip node-1` METRIC 2
-docker-machine ls
+
+vagrant ssh -c 'sudo ip addr del 192.168.33.10/24 dev eth1'
+vagrant ssh -c 'sudo docker network create  --driver bridge    --subnet=192.168.33.0/24    --gateway=192.168.33.10   --opt "com.docker.network.bridge.name"="docker1"  shared_nw'
+vagrant ssh -c 'sudo brctl addif docker1 eth1' 
+eval $(docker-machine env host1)
+
+docker-machine ssh host1 "mkdir ./haproxy"
+scp haproxy/result_haproxy.cfg vagrant@192.168.33.10:./haproxy/haproxy.cfg
+scp src/*.yml vagrant@192.168.33.10:.
+scp docker-compose vagrant@192.168.33.10:.
+docker-machine ssh host1 "sudo usermod -aG docker vagrant"
+  
+#docker-machine ssh node-1 "docker stack deploy -c docker-compose.yml getstartedlab"
+docker-machine ssh host1 "docker-compose -f docker-compose_network.yml up"
+
+#route delete 172.18.0.0
+#route add 172.18.0.0 MASK 255.255.255.0 `docker-machine ip node-1` METRIC 2
+#docker-machine ls
 
 
 cd ${currentfodler}
