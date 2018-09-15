d87538cac2f8bfaf96cbbc581db00f35f48f979f
Harry <Harryyhzhang@gmail.com>
2018-09-14 21:10:10 -0400

    add R sutdio into dockerfile

--- go.sh
+++ go.sh
@@ -23,7 +23,10 @@ vagrant ssh -c 'sudo ip addr del 192.168.33.10/24 dev eth1'
 vagrant ssh -c 'sudo docker network create  --driver bridge    --subnet=192.168.33.0/24    --gateway=192.168.33.10   --opt "com.docker.network.bridge.name"="docker1"  shared_nw'
 vagrant ssh -c 'sudo brctl addif docker1 eth1' 
 eval $(docker-machine env host1)
-
+docker-machine ssh host1 "sudo usermod -aG docker vagrant"
+scp -r -i ../dataplatform/.vagrant/machines/host1/virtualbox/private_key ../dataplatform vagrant@192.168.33.10:~/
+ ######################################3
+ 
 docker-machine ssh host1 "mkdir ./haproxy"
 scp haproxy/result_haproxy.cfg vagrant@192.168.33.10:./haproxy/haproxy.cfg
 scp src/*.yml vagrant@192.168.33.10:.
@@ -31,7 +34,7 @@ scp src/*.yml vagrant@192.168.33.10:.
 scp docker-compose vagrant@192.168.33.10:.
 scp src/hadoop-hive.env vagrant@192.168.33.10:.
 scp src/hadoop.env vagrant@192.168.33.10:.
-docker-machine ssh host1 "sudo usermod -aG docker vagrant"
+
   
 #docker-machine ssh node-1 "docker stack deploy -c docker-compose.yml getstartedlab"
 docker-machine ssh host1 "docker-compose -f docker-compose_network.yml up"
@@ -42,3 +45,8 @@ docker-machine ssh host1 "docker-compose -f docker-compose_network.yml up"
 
 
 cd ${currentfodler}
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker-compose -f docker-compose_hive.yml up
