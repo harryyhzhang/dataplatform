625a297aacf8045bc79cdabeb4eac668e74b3940
Harry <Harryyhzhang@gmail.com>
2018-09-25 23:16:16 -0400

    change yml to start hive cluster

--- go.sh
+++ go.sh
@@ -25,15 +25,17 @@ vagrant ssh -c 'sudo brctl addif docker1 eth1'
 eval $(docker-machine env host1)
 docker-machine ssh host1 "sudo usermod -aG docker vagrant"
 scp -r -i ../dataplatform/.vagrant/machines/host1/virtualbox/private_key ../dataplatform vagrant@192.168.33.10:~/
+vagrant ssh -c 'chmod +x ~/dataplatform/docker-compose'
+docker-machine ssh host1 " ~/dataplatform/docker-compose -f src/docker-compose_rstudioserver2.yml up "
  ######################################3
  
-docker-machine ssh host1 "mkdir ./haproxy"
-scp haproxy/result_haproxy.cfg vagrant@192.168.33.10:./haproxy/haproxy.cfg
-scp src/*.yml vagrant@192.168.33.10:.
+#docker-machine ssh host1 "mkdir ./haproxy"
+#scp haproxy/result_haproxy.cfg vagrant@192.168.33.10:./haproxy/haproxy.cfg
+#scp src/*.yml vagrant@192.168.33.10:.
 # scp -r dataplatform vagrant@192.168.33.10:~/
-scp docker-compose vagrant@192.168.33.10:.
-scp src/hadoop-hive.env vagrant@192.168.33.10:.
-scp src/hadoop.env vagrant@192.168.33.10:.
+#scp docker-compose vagrant@192.168.33.10:.
+#scp src/hadoop-hive.env vagrant@192.168.33.10:.
+#scp src/hadoop.env vagrant@192.168.33.10:.
 
   
 #docker-machine ssh node-1 "docker stack deploy -c docker-compose.yml getstartedlab"
@@ -44,9 +46,9 @@ docker-machine ssh host1 "docker-compose -f docker-compose_network.yml up"
 #docker-machine ls
 
 
-cd ${currentfodler}
+#cd ${currentfodler}
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
-sudo docker-compose -f docker-compose_hive.yml up
+#sudo docker-compose -f docker-compose_hive.yml up
