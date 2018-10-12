5fbd3d8d1c547d8590292eb67b6b7e1e582a6cdd
Harry <Harryyhzhang@gmail.com>
2018-10-12 14:27:56 -0400

    add the compose syntax

--- gofromlinux.sh
+++ gofromlinux.sh
@@ -4,7 +4,7 @@ source ./ops.sh
 cd src 
 chmod +x docker-compose 
 sudo docker network create  --driver bridge    --subnet=192.168.33.0/24    --gateway=192.168.33.10   --opt "com.docker.network.bridge.name"="docker1"  shared_nw 
-~/dataplatform/src/docker-compose -f docker-compose_rstudioserver2.yml up -d 
+./docker-compose -f docker-compose_rstudioserver2.yml up -d  || docker-compose -f docker-compose_rstudioserver2.yml up -d
 gocontainer 0 "/usr/local/hadoop/spark-services.sh"
 
  
